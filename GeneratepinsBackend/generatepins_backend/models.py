5d46db191a99aeac63e0585452f570156c2cc18e
Andyphied <philipandy2@gmail.com>
2021-05-26 15:24:49 +0100

    Renamed and restructured model

--- GeneratepinsBackend/generatepins_backend/models.py
+++ GeneratepinsBackend/generatepins_backend/models.py
@@ -2,11 +2,13 @@ from sqlalchemy import func
 from generatepins_backend.db import db
 
 
-class GeneratepinModel(db.Model):
+class PinGenModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    phone_number = db.Column(db.String(250))
     pin = db.Column(db.String(50))
-    used = db.Column(db.Boolean(), default=False)
+    verified = db.Column(db.Boolean(), default=False)
     type = db.Column(db.Integer())
     count = db.Column(db.Integer(), default=1)
-    expiry_time = db.Column(db.DateTime, server_default=func.now())
+    expiry_time = db.Column(db.DateTime)
+    timestamp = db.Column(db.DateTime, server_default=func.now())
