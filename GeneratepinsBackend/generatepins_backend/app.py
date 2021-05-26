6073d4703df8fea9383f1be33474f41b73706058
Andyphied <philipandy2@gmail.com>
2021-05-26 15:23:59 +0100

    Renamed APi

--- GeneratepinsBackend/generatepins_backend/app.py
+++ GeneratepinsBackend/generatepins_backend/app.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_restplus import Api
 
 
-
 def create_app():
     from generatepins_backend.api_namespace.api import api_namespace
 
     application = Flask(__name__)
-    api = Api(application, version='0.1', title='Generatepins Backend API',
-              description='A Simple CRUD API')
+    api = Api(application,
+              version='1.1',
+              title='Pin Generation API',
+              description='Generate Otp and Pins')
 
     from generatepins_backend.db import db, db_config
     application.config['RESTPLUS_MASK_SWAGGER'] = False
